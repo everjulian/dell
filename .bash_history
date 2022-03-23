accc0c8ca35865e433a221ea3780ddc89daaa8f4
everjulian <everarboleda9@gmail.com>
2022-03-23 10:35:21 -0500

    Ever

---
+++ .bash_history
@@ -0,0 +1,15 @@
+cd Escritorio 
+cd..
+cd
+cd(Escritorio)
+cd / Escritorio
+cd Escritorio 
+cd
+cd Documentos 
+cd ..
+cd PROYECTO INTEGRADOR
+Cd PROYECTO 
+is
+IS
+git inite
+git --help
---
+++ Desktop/matrix.bat
@@ -0,0 +1,17 @@
+@echo off
+title Matrix
+color 0a
+mode 1000
+goto Greeting
+
+
+:Greeting
+cls
+echo Matrix
+
+cls
+goto Matrix
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto Matrix
